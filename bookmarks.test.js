74e5ac94ce5d6c7960ad18dc8aa9cee630a5b482
theangelskies <theangelskies13@yahoo.com>
2026-02-21 20:21:28 +0100

    implement mock localStorage for testing environment

--- bookmarks.test.js
+++ bookmarks.test.js
@@ -1,5 +1,21 @@
 import assert from "node:assert";
 import test from "node:test";
+global.localStorage = {
+  store: {},
+  getItem(key) {
+    return this.store[key] || null;
+  },
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  },
+  removeItem(key) {
+    delete this.store[key];
+  },
+  clear() {
+    this.store = {};
+  },
+};
+
 import {
   getUsers,
   getBookmarks,
@@ -7,7 +23,7 @@ import {
   likeBookmark,
 } from "./bookmarks.js";
 import { getUserIds, getData, clearData } from "./storage.js";
-
+const TEST_USER = "1";
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
 });
